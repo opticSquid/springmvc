85104a46cf0e9128f713b5ee329b6f70bb5ef4aa
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-05-21 11:55:59 +0530

    Using model map and String view to make code more readable
    This decouples model from view when there is no work for data
    transfer from controller to UI and vice versa and this also makes the
    code more concise.

--- src/main/java/com/sb/spring/springmvc/controller/UserController.java
+++ src/main/java/com/sb/spring/springmvc/controller/UserController.java
@@ -1,28 +1,25 @@
 package com.sb.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.sb.spring.springmvc.dto.User;
 
 @Controller
 public class UserController {
 	@RequestMapping("registrationPage")
-	public ModelAndView showRegistration() {
-		ModelAndView modelandview = new ModelAndView();
-		modelandview.setViewName("userReg");
-		return modelandview;
+	public String showRegistrationPage() {
+		// Returning just the view name as String not any model
+		return "userReg";
 	}
 
 	@RequestMapping(value = "registerUser", method = RequestMethod.POST)
-	public ModelAndView registerUser(@ModelAttribute("user") User user) {
+	public String registerUser(@ModelAttribute("user") User user, ModelMap model) {
 		System.out.println(user);
-		ModelAndView modelandview = new ModelAndView();
-		modelandview.setViewName("regSuccess");
-		modelandview.addObject(user);
-		return modelandview;
+		model.addAttribute("user", user);
+		return "regSuccess";
 	}
 }
