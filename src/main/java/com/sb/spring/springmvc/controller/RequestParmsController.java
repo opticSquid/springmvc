fc13f7b9a8c20a1b01b9c855fb155b7e1c17c415
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-05-21 11:30:29 +0530

    Steps to make some query parameters optional was done here

--- src/main/java/com/sb/spring/springmvc/controller/RequestParmsController.java
+++ src/main/java/com/sb/spring/springmvc/controller/RequestParmsController.java
@@ -12,11 +12,18 @@ public class RequestParmsController {
 
 	@RequestMapping("/showData")
 	public ModelAndView showData(@RequestParam("id") int id, @RequestParam("name") String name,
-			@RequestParam("email") String email) {
+			@RequestParam(value = "email", required = false) String email) {
+		// By default every query param is required so to make some query param optional
+		// we have to provide required=false and and add value="<param_key>" in
+		// RequestParam annotation
+		// if the data type is a primitive type and no param value comes in for an
+		// optional parameter it results in an error saying null can not be assigned to
+		// a primitive data type. In that case you have to provide another parameter
+		// called "defaultValue"="some value of same data type"
 		ModelAndView modelandview = new ModelAndView();
 		System.out.println("Id: " + id);
 		System.out.println("Name: " + name);
-		System.out.println("Email" + email);
+		System.out.println("Email: " + email);
 		modelandview.setViewName("regSuccess");
 		User user = new User();
 		user.setId(id);
