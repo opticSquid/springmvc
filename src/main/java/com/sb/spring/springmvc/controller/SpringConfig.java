36d650b0acd02b9e222acac004465ad5ca2fd5b1
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-05-25 11:10:58 +0530

    Testrun1: removing dispatcher-servlet in favour of SpringConfig

---
+++ src/main/java/com/sb/spring/springmvc/controller/SpringConfig.java
@@ -0,0 +1,29 @@
+package com.sb.spring.springmvc.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan("com.sb.spring.springmvc.controller")
+@Configuration
+public class SpringConfig extends WebMvcConfigurationSupport {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	@Override
+	protected void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
